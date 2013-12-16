34871e193c8ca3af22b203ef89988ad1ba3dec56
JorgenScott <Jorgen@Hookham.co>
2013-12-16 16:58:10 +0000

    Create yo-todo project, create add todo functionality.

---
+++ yo-todo/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yoTodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+  });
